8e5d1dc5236c9a32b8beab4d6a6df9f7500a0902
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-10 15:03:30 +0800

    Update class-theridgebali-core-public.php

--- public/class-theridgebali-core-public.php
+++ public/class-theridgebali-core-public.php
@@ -96,7 +96,7 @@ class Theridgebali_Core_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-		wp_enqueue_script( $this->plugin_name., plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), $this->version, true);
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), $this->version, true);
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/theridgebali-core-public.js', array( 'jquery' ), $this->version, false );
 
 	}
