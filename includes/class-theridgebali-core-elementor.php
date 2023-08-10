5a2ef153361be0c37804f1cc7ee948c10b287cf4
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-10 14:49:28 +0800

    Update class-theridgebali-core-elementor.php

--- includes/class-theridgebali-core-elementor.php
+++ includes/class-theridgebali-core-elementor.php
@@ -54,7 +54,7 @@ function theridgebali_core_stylesheets() {
 add_action( 'elementor/frontend/after_enqueue_styles', 'theridgebali_core_stylesheets' );
 
 function theridgebali_core_scripts() {
-    wp_register_script( 'bootstrap-script', plugins_url( 'public/js/bootstrap.bundle.min.js', __FILE__ ), array( 'jquery' ), null, true );
+    wp_register_script( 'bootstrap-script', plugins_url( 'public/js/bootstrap.bundle.min.js', __FILE__ ), array( 'jquery' ), 99, true );
     wp_enqueue_script( 'bootstrap-script' );
 }
 add_action( 'wp_enqueue_scripts', 'theridgebali_core_scripts' );
