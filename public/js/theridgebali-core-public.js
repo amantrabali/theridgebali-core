ef225e49a11f1932a65b95fbe79050eb13b4b427
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-08 14:43:56 +0800

    Update theridgebali-core-public.js

--- public/js/theridgebali-core-public.js
+++ public/js/theridgebali-core-public.js
@@ -50,5 +50,24 @@
 	    navbarRidge();
 	});
 
+	// Select the header element with the class .nav-sticky
+	const navSticky = document.querySelector('.nav-sticky');
+
+	// Function to add the .navigation-sticky-active class
+	function addStickyClass() {
+	  // Check if the user has scrolled 50 pixels or more
+	  if (window.scrollY >= 50) {
+	    // Add the .navigation-sticky-active class
+	    navSticky.classList.add('navigation-sticky-active');
+	  } else {
+	    // Remove the .navigation-sticky-active class if not scrolled 50 pixels
+	    navSticky.classList.remove('navigation-sticky-active');
+	  }
+	}
+
+	// Add an event listener to the window's scroll event
+	window.addEventListener('scroll', addStickyClass);
+
+
 
 })( jQuery );
