686c55a1b2bf0e857b29826729766a7b2278f841
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-16 00:45:59 +0800

    Update class-theridgebali-core-elementor.php

--- includes/class-theridgebali-core-elementor.php
+++ includes/class-theridgebali-core-elementor.php
@@ -43,7 +43,6 @@ function register_theridgebali_widget( $widgets_manager ) {
 
     require_once( __DIR__ . '/widgets/the-ridge-widget-1.php' );
     require_once( __DIR__ . '/widgets/the-ridge-widget-grid-blog.php' );
-    require_once( __DIR__ . '/widgets/the-ridge-widget-grid-blog_archive.php' );
     require_once( __DIR__ . '/widgets/the-ridge-widget-footer-list.php' );
     require_once( __DIR__ . '/widgets/the-ridge-widget-footer-text.php' );
     require_once( __DIR__ . '/widgets/the-ridge-widget-hamburger-menu.php' );
@@ -52,7 +51,6 @@ function register_theridgebali_widget( $widgets_manager ) {
 
     $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_1() );
     $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_Grid_Blog() );
-    $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_Grid_Blog_Archive() );
     $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_Footer_List() );
     $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_Footer_Text() );
     $widgets_manager->register( new \Elementor_TheRidgeBali_Widget_Hamburger_Menu() );    
