ad9894b8aabf44f1695aead8d3ea729877917d20
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-11-28 23:37:03 +0800

    Update the-ridge-widget-hamburger-menu.php

--- includes/widgets/the-ridge-widget-hamburger-menu.php
+++ includes/widgets/the-ridge-widget-hamburger-menu.php
@@ -33,33 +33,11 @@ class Elementor_TheRidgeBali_Widget_Hamburger_Menu extends \Elementor\Widget_Bas
 			]
 		);
 
-		$this->add_control(
-			'hamburger_color',
-			[
-				'label' => esc_html__( 'Hamburger Color', 'theridgebali-core' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .menu-main-color' => 'color: {{VALUE}}',
-				],
-			]
-		);
-
-		$this->add_control(
-			'hamburger_sticky_color',
-			[
-				'label' => esc_html__( 'Hamburger Sticky Color', 'theridgebali-core' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .menu-main-sticky-color' => 'color: {{VALUE}}',
-				],
-			]
-		);
-
 		$this->add_responsive_control(
-			'space_between',
+			'hamburger_main_menu',
 			[
 				'type' => \Elementor\Controls_Manager::SLIDER,
-				'label' => esc_html__( 'Menu Width', 'textdomain' ),
+				'label' => esc_html__( 'Main Menu Width', 'theridgebali-core' ),
 				'range' => [
 					'px' => [
 						'min' => 0,
@@ -85,6 +63,36 @@ class Elementor_TheRidgeBali_Widget_Hamburger_Menu extends \Elementor\Widget_Bas
 			]
 		);
 
+		$this->add_responsive_control(
+			'hamburger_close_menu',
+			[
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'label' => esc_html__( 'Close Menu Width', 'theridgebali-core' ),
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'devices' => [ 'desktop', 'tablet', 'mobile' ],
+				'desktop_default' => [
+					'size' => 30,
+					'unit' => 'px',
+				],
+				'tablet_default' => [
+					'size' => 20,
+					'unit' => 'px',
+				],
+				'mobile_default' => [
+					'size' => 10,
+					'unit' => 'px',
+				],
+				'selectors' => [
+					'{{WRAPPER}} .hamburger-close-menu' => 'width: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
 
 		$this->end_controls_section();
 
@@ -101,7 +109,7 @@ class Elementor_TheRidgeBali_Widget_Hamburger_Menu extends \Elementor\Widget_Bas
 		<div id="topnav" class="topnav">
         <div class="col-md-3 col-xs-3">
               <div class="nav-trigger">
-	                <svg class="hamburger-main-menu menu-main-color" width="100%" height="100%" viewBox="0 0 50 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+	                <svg class="hamburger-main-menu" width="100%" height="100%" viewBox="0 0 50 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path class="svg-path" d="M0 1.45455V0H50V1.45455H0ZM50 7.27273V8.72727H0V7.27273H50ZM0 14.5455H50V16H0V14.5455Z" fill="none"/>
 									</svg>
               </div>
@@ -112,7 +120,7 @@ class Elementor_TheRidgeBali_Widget_Hamburger_Menu extends \Elementor\Widget_Bas
         <aside id="navigation">
 		    <div class="inner">
 		        <div class="nav-trigger">
-		            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+		            <svg class="hamburger-close-menu" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path class="svg-path" d="M29.7688 28.6524C29.8421 28.7257 29.9003 28.8127 29.9399 28.9085C29.9796 29.0043 30 29.1069 30 29.2106C30 29.3143 29.9796 29.4169 29.9399 29.5127C29.9002 29.6085 29.842 29.6955 29.7687 29.7688C29.6954 29.8421 29.6084 29.9003 29.5126 29.9399C29.4168 29.9796 29.3141 30 29.2105 30C29.1068 30 29.0041 29.9796 28.9084 29.9399C28.8126 29.9002 28.7256 29.842 28.6523 29.7687L15 16.1166L1.34774 29.7687C1.1997 29.9168 0.998903 30 0.789526 30C0.580149 30 0.37934 29.9169 0.231275 29.7688C0.08321 29.6208 1.81397e-05 29.42 2.96612e-09 29.2106C-1.81338e-05 29.0012 0.0831393 28.8004 0.231178 28.6524L13.8835 15L0.231178 1.34764C0.0831393 1.19957 -1.81338e-05 0.998766 2.96612e-09 0.789389C8.98338e-06 0.685716 0.0204377 0.58306 0.06012 0.487282C0.0998022 0.391504 0.157961 0.30448 0.231275 0.231178C0.304589 0.157877 0.391623 0.0997337 0.487408 0.060068C0.583193 0.0204024 0.685853 -8.97745e-06 0.789526 2.96206e-09C0.998903 1.81397e-05 1.1997 0.08321 1.34774 0.231275L15 13.8834L28.6523 0.231275C28.7256 0.157961 28.8126 0.0998022 28.9084 0.06012C29.0041 0.0204377 29.1068 8.98338e-06 29.2105 2.96206e-09C29.3141 -8.97745e-06 29.4168 0.0204024 29.5126 0.060068C29.6084 0.0997337 29.6954 0.157877 29.7687 0.231178C29.842 0.30448 29.9002 0.391504 29.9399 0.487282C29.9796 0.58306 30 0.685716 30 0.789389C30 0.893062 29.9796 0.995722 29.9399 1.09151C29.9003 1.18729 29.8421 1.27433 29.7688 1.34764L16.1165 15L29.7688 28.6524Z" fill="none"/>
 								</svg>
 		        </div>
